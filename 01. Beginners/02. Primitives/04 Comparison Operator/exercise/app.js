2485a23c4a96fa814ea57e9eff9491ec87d03ba1
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-19 14:51:29 +0500

    Compasison & equlity operator

--- 01. Beginners/02. Primitives/04 Comparison Operator/exercise/app.js
+++ 01. Beginners/02. Primitives/04 Comparison Operator/exercise/app.js
@@ -1,23 +1,34 @@
 // 1. Create variable name (firstFavNumb) & store your favorite number.
+let firstFavNumb=7;
 // 2. Create variable name (secondFavNumb) & store second fav Number.
+let secondFavNumb=16;
 // 3. Check (firstFavNumb is greater then secondFavNumb)
-// 4. Check (firstFavNumb is less then secondFavNumb)
-// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
-// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
-// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
-// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
-// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
-// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
-
-let firstFavNumb = 21;
-let secondFavNumb = 10;
-
 console.log(firstFavNumb > secondFavNumb);
-console.log(firstFavNumb < secondFavNumb);
-console.log(firstFavNumb >= secondFavNumb);
-console.log(firstFavNumb <= secondFavNumb);
 
+// 4. Check (firstFavNumb is less then secondFavNumb)
+console.log(firstFavNumb < secondFavNumb);
+
+// 5. Check (firstFavNumb is greater then & equal to secondFavNumb)
+console.log(firstFavNumb >= secondFavNumb);
+// 6. Check (firstFavNumb is less then & equal to secondFavNumb)
+console.log(firstFavNumb <= secondFavNumb);
+// 7. Check (firstFavNumb is equal to secondFavNumb) using strict equality operator.
 console.log(firstFavNumb === secondFavNumb);
+// 8. Check (firstFavNumb is equal to secondFavNumb) using loose equality operator.
 console.log(firstFavNumb !== secondFavNumb);
+// 9. Check (firstFavNumb is not equal to secondFavNumb) using strict non-equality operator.
 console.log(firstFavNumb == secondFavNumb);
+// 10. Check is (firstFavNumb is not equal to secondFavNumb) using loose non-equality operator.
 console.log(firstFavNumb != secondFavNumb);
+// let firstFavNumb = 21;
+// let secondFavNumb = 10;
+
+// console.log(firstFavNumb > secondFavNumb);
+// console.log(firstFavNumb < secondFavNumb);
+// console.log(firstFavNumb >= secondFavNumb);
+// console.log(firstFavNumb <= secondFavNumb);
+
+// console.log(firstFavNumb === secondFavNumb);
+// console.log(firstFavNumb !== secondFavNumb);
+// console.log(firstFavNumb == secondFavNumb);
+// console.log(firstFavNumb != secondFavNumb);
