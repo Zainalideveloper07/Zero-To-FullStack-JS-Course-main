85b9ab5e02e611075086dddb50c7d9b8fe596967
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-19 12:31:27 +0500

    booleans

--- 01. Beginners/02. Primitives/03 Booleans/exercise/app.js
+++ 01. Beginners/02. Primitives/03 Booleans/exercise/app.js
@@ -1,11 +1,19 @@
 // 1. Create Variable name (isJsProgrammingLanguage) & store true as a value.
-// 2. Create variable name (isJsHard) & store false as a value.
-// 3. Create variable name (favNumb) & store your favorite number inside.
-// 4. Now Add favNumber with the value of undefined.
+let isJsProgrammingLanguage=true;
 
-let isJsProgrammingLanguage = true;
-let isJsHard = false;
-let favNumb = 21;
+// 2. Create variable name (isJsHard) & store false as a value.
+let isJsHard=false;
+// 3. Create variable name (favNumb) & store your favorite number inside.
+let favNumb=22
+// 4. Now Add favNumber with the value of undefined.
 console.log(isJsProgrammingLanguage);
 console.log(isJsHard);
-console.log(favNumb + undefined);
+console.log(favNumb+undefined);
+
+
+// let isJsProgrammingLanguage = true;
+// let isJsHard = false;
+// let favNumb = 21;
+// console.log(isJsProgrammingLanguage);
+// console.log(isJsHard);
+// console.log(favNumb + undefined);
