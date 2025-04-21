5307c21acb124e119d5795c0636aec6ac22e8c00
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-21 16:50:56 +0500

    object Exersic done

--- 01. Beginners/04. Reference Types (Basics)/03. Objects/exercise/app.js
+++ 01. Beginners/04. Reference Types (Basics)/03. Objects/exercise/app.js
@@ -1,5 +1,5 @@
 // 1. Create object name (car)
-const car={};
+let car={};
 // 2. add (type, model, color) as properties
 car ={
 type:"",
@@ -7,9 +7,13 @@ model:"",
 color:""
 };
 // 3. check the type of object
+console.log(typeof car);
 // 4. Update the type property to "Toyota"
+car.type="Toyota";
 // 5. Add new property wheels
+car.wheele=4;
 // 6. log car to the console.
+console.log(car);
 
 // const car = { type: "Fiat", model: "500", color: "white" };
 
@@ -23,3 +27,6 @@ color:""
 // // Add new properties
 // console.log((car.wheels = 4));
 // console.log(car);
+delete car.wheele;
+console.log(car);
+
