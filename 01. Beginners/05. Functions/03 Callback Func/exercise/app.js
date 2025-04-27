05bc27d45243b5bde1d38527cfe6ee12b0f27d63
zainalideveloper07 <zainalideveloper07@gmail.com>
2025-04-27 19:06:06 +0500

    Push

--- 01. Beginners/05. Functions/03 Callback Func/exercise/app.js
+++ 01. Beginners/05. Functions/03 Callback Func/exercise/app.js
@@ -4,6 +4,58 @@
 // 4. pass that value to the callback function
 // 5. Now create callback function & print that value which you pass as parameter to the callback func.
 
+// function greet(name ,callback){
+//     console.log(`Hello  ${name}`);
+//     callback();
+    
+// };
+
+// function sayGoodBye(){
+//     console.log("Goodbye");
+    
+// };
+
+// greet("Zain",sayGoodBye);
+
+
+function doMath(x,y,Callback){
+    return Callback(x,y);
+}
+
+function add(x,y){
+    console.log(x+y);
+}
+
+function sub(x,y){
+    console.log(x-y);
+}
+
+function devied(x,y){
+    console.log(x/y);
+}
+
+function Multiply(x,y){
+    console.log(x*y);
+}
+console.log("Multiply");
+
+doMath(5,2,Multiply);
+doMath(2,22,Multiply);
+doMath(5,2,Multiply);
+console.log("add");
+doMath(5,2,add);
+doMath(2,22,add);
+doMath(5,2,add);
+console.log("devied");
+doMath(5,2,devied);
+doMath(2,22,devied);
+doMath(5,2,devied); 
+
+
+
+
+
+
 // function fn(){
 //   console.log(value);
 // }
